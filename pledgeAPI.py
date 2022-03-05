3effd60f6483e1cbd0057a1e57c408bb1af2c1b6
Ishan Gohil <ishangohil@res1-139-98-dhcp.int.colorado.edu>
2022-03-05 02:01:19 -0700

    test

--- pledgeAPI.py
+++ pledgeAPI.py
@@ -5,6 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
 @app.route("/getmethod/<jsdata>")
 def getJSData():
     headers = {"Authorization": "Bearer 4e1b9114e2a8f946fe095f961b3a3d88"}
