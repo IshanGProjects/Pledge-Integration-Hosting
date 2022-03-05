ecfb533c2e0ff02584e6337996f944e55fd2fc23
Ishan Gohil <ishangohil@cu-biot-11-10.203.170.22.int.colorado.edu>
2022-03-05 10:28:09 -0700

    removed api stuff to test that python is working properly

--- pledgeAPI.py
+++ pledgeAPI.py
@@ -10,11 +10,11 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/getmethod/<jsdata>")
-def getJSData():
-    headers = {"Authorization": "Bearer 4e1b9114e2a8f946fe095f961b3a3d88"}
-    r = requests.get(
-        "https://api.pledge.to/v1/organizations",
-        headers=headers
-    ).json()
-    return r
+# @app.route("/getmethod/<jsdata>")
+# def getJSData():
+#     headers = {"Authorization": "Bearer 4e1b9114e2a8f946fe095f961b3a3d88"}
+#     r = requests.get(
+#         "https://api.pledge.to/v1/organizations",
+#         headers=headers
+#     ).json()
+#     return r
