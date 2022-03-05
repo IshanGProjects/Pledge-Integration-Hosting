583939b58998c4f59db49e47e081816f567f7c54
Ishan Gohil <86436938+IshanGProjects@users.noreply.github.com>
2022-03-05 00:28:40 -0700

    Add files via upload
    This is a file that creates a request for the api and then returns it to be processed by the HTML, CSS and JS

---
+++ pledgeAPI.py
@@ -0,0 +1,15 @@
+from pickle import NONE
+import requests
+import json
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/getmethod/<jsdata>")
+def getJSData():
+    headers = {"Authorization": "Bearer 4e1b9114e2a8f946fe095f961b3a3d88"}
+    r = requests.get(
+        "https://api.pledge.to/v1/organizations",
+        headers=headers
+    ).json()
+    return r
